ceedb611e531066db036808d192fecbadba9364e
Vish Desai <shadyvd@hotmail.com>
2020-04-20 17:49:22 +0530

    chore(commitizen): added .czrc file
    Moved commitizen config out of package.json and into its own file

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,51 +4,51 @@ const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function () {
 	return {
-		"scenarios": [
-			{
-				name: 'ember-lts-3.16',
-				npm: {
-					devDependencies: {
-						'ember-source': '~3.16.0'
+		"scenarios": [{
+				"name": 'ember-lts-3.16',
+				"npm": {
+					"devDependencies": {
+						"ember-source": '~3.16.0'
 					}
 				}
 			},
 			{
-				name: 'ember-release',
-				npm: {
-					devDependencies: {
-						'ember-source': await getChannelURL('release')
+				"name": 'ember-release',
+				"npm": {
+					"devDependencies": {
+						"ember-source": await getChannelURL('release')
 					}
 				}
 			},
 			{
-				name: 'ember-beta',
-				npm: {
-					devDependencies: {
-						'ember-source': await getChannelURL('beta')
+				"name": 'ember-beta',
+				"npm": {
+					"devDependencies": {
+						"ember-source": await getChannelURL('beta')
 					}
 				}
 			},
 			{
-				name: 'ember-canary',
-				npm: {
-					devDependencies: {
-						'ember-source': await getChannelURL('canary')
+				"name": 'ember-canary',
+				"npm": {
+					"devDependencies": {
+						"ember-source": await getChannelURL('canary')
 					}
 				}
 			},
 			{
-				name: 'ember-classic',
-				env: {
-					EMBER_OPTIONAL_FEATURES: JSON.stringify({
-						'application-template-wrapper': true,
-						'default-async-observers': false,
-						'template-only-glimmer-components': false
+				"name": 'ember-classic',
+				"env": {
+					"EMBER_OPTIONAL_FEATURES": JSON.stringify({
+						"application-template-wrapper": true,
+						"default-async-observers": false,
+						"jquery-integration": true,
+						"template-only-glimmer-components": false
 					})
 				},
-				npm: {
-					ember: {
-						edition: 'classic'
+				"npm": {
+					"ember": {
+						"edition": 'classic'
 					}
 				}
 			}
